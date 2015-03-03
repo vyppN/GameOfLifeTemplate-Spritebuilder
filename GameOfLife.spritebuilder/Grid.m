9a88999d83c76611010b840f3806ca6cb6f484cf
vyppN <vyppN@Chaowarits-MacBook-Pro.local>
2015-03-03 10:10:40 +0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -60,8 +60,8 @@ static const int GRID_COLS = 10;
 }
 
 -(Creature*)creatureForTouchPosition:(CGPoint)touchPosition{
-    int row = touchPosition.y/_cellHeight;
-    int col = touchPosition.x/_cellWidth;
+    int row = _cellHeight/touchPosition.y;
+    int col = _cellWidth/touchPosition.x;
     return _gridArray[row][col];
 }
 
